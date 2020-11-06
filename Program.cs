d386c9e3210dcbd05fb6bd4b22f5b8598a98adbc
oliveiraFab16 <fabriciocarvalho932@gmail.com>
2020-11-06 16:02:12 -0300

    DoandoSangue

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace DoadorDeSangue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+            string nome;
+           
+            Console.WriteLine("--- Pelo Horario De Brasília Agora São:");
+            DateTime horaSistema = DateTime.Now;
+            Console.WriteLine($"Hora Do Sistema: {horaSistema}");
+        
+
+            Console.Write("Digite seu nome: ");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Digite sua idade: ");
+            idade = int.Parse(Console.ReadLine());
+                    
+            if (idade <= 18)
+            {
+              Console.WriteLine($"Nome:{nome}");
+              Console.WriteLine("Você Não Pode Ser Doador(a) De Sangue");
+              Console.WriteLine("A Idade Minima Para doar É De 18 Anos!");
+              Console.WriteLine("Pressione ENTER para sair!");
+              Console.ReadKey();
+            }
+            if (idade >= 18 && idade <= 67)
+            {
+                Console.WriteLine($"Nome:{nome}");
+                Console.WriteLine("Você Pode Ser Doador(a) De Sangue");
+                Console.WriteLine("A Idade Minima Para doar É De 18 Anos");
+                Console.WriteLine("Pressione ENTER para sair!");
+                Console.ReadKey();
+            }
+            if (idade  >= 67 && idade <= 84 ) 
+            {
+              Console.WriteLine($"Nome:{nome}");
+              Console.WriteLine("Você Não Pode Ser Doador(a) De Sangue");
+              Console.WriteLine("A Idade Minima Para doar É De 18 Anos");
+              Console.WriteLine("Pressione ENTER para sair!");
+              Console.ReadKey();
+            }
+              
+        }
+    }
+}
